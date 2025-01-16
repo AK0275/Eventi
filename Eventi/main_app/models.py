4a17e5c867376595d75d3334f08c04cc10aa6776
AK0275 <abdullakhalid0275@gmail.com>
2025-01-16 14:07:16 +0300

    category models

--- Eventi/main_app/models.py
+++ Eventi/main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+Social = 'Social'
+Sports = 'Sports'
+Foods = 'Foods'
+Festivals = 'Festivals'
+Outdoor = 'Outdoor'
+Other = 'Other'
+
+
+EVENT_TYPE = [
+        (Social, 'Social'),
+        (Sports, 'Sports'),
+        (Foods, 'Foods'),
+        (Festivals, 'Festivals'),
+        (Outdoor, 'Outdoor'),
+        (Other, 'Other'),
+    ]
+
+class category(models.Model):
+    type = models.CharField(max_length=100, choices=EVENT_TYPE, default=Social,)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'category_id': self.id})
+
+
+    def __str__(self):
+        return self.type
+
+
